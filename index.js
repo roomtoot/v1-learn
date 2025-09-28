de562922ab614ce7d46202ab89ce82a8e93a4daf
roomtoot <jeckgreker@gmail.com>
2025-09-28 13:08:49 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,20 +1,38 @@
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
+const numberOfFilms = +prompt('Сколько фильмов вы уже помотрели?','');
 
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
-    actors: {},
-    genres: [],
+    actors:{},
+    genres:[],
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных филмов?',''),
-      b = prompt('На сколько оцените его?',''),
-      c = prompt('Один из последних просмотренных филмов?',''),
-      d = prompt('На сколько оцените его?','');
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?',''),
+      b = prompt('На сколько оцените его?','');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+         personalMovieDB.movies[a] = b ; 
+         console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    
+    
+   
+}
+
+if (personalMovieDB.count < 1){
+    console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 1 && personalMovieDB < 3){
+    console.log('вы классический зритель');
+} else if (personalMovieDB.count >= 3){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(personalMovieDB);
